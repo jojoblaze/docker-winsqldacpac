6b8e303429d7ca6c17a4b9c70e5d6a075392fe34
Dello Ioio <Matteo.DelloIoio@wolterskluwer.com>
2020-02-14 14:03:08 +0100

    first commit

---
+++ MSSQLDB/dbo/Tables/weather-forecast.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[weather-forecast]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [date] DATETIME2 NOT NULL, 
+    [temperature_celsius] SMALLINT NOT NULL, 
+    [summary] NVARCHAR(50) NOT NULL
+)
